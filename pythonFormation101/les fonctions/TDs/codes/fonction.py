7b47c9eb9d10cc7a63db295dc971e5ae82940bf7
Bekkali Hamza <bakkalihamza2014@gmail.com>
2025-01-26 19:54:10 +0000

    #

--- pythonFormation101/les fonctions/TDs/codes/fonction.py
+++ pythonFormation101/les fonctions/TDs/codes/fonction.py
@@ -1,7 +1,7 @@
 
 def indice(A, val):
     id=0
-    while A[id] > val or val > =A[id+1]:
+    while A[id] > val or val >=  A[id+1]:
         id+=1
     return id
 # suppose que id existe toujours sinon
