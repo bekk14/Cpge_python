3bdbc53a47fa56910c9d1c8d57074881ccc93ead
Bekkali Hamza <bakkalihamza2014@gmail.com>
2025-01-30 20:11:59 +0100

    Delete pythonFormation101 directory from main

--- pythonFormation101/les fonctions/TDs/codes/fonction.py
+++
@@ -1,8 +0,0 @@
-
-def indice(A, val):
-    id=0
-    while A[id] > val or val >=  A[id+1]:
-        id+=1
-    return id
-# suppose que id existe toujours sinon
-# la boucle pourrait ne pas se terminer
