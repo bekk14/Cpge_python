96d3e12526ea7a80fd7df43316207599c9b796a5
Bekkali Hamza <bakkalihamza2014@gmail.com>
2025-02-18 16:18:08 +0000

    update

--- pythonFormation101/les fonctions/TDs/TD5_022025/exercice2.py
+++ pythonFormation101/les fonctions/TDs/TD5_022025/exercice2.py
@@ -1,24 +1,65 @@
-
-# Q a 
+"""
+On considère la fonction mystere(n) ci-dessous
+a : Décrire ce que fait cette fonction et proposer une autre solution.
+b : Déterminer la complexité de mystere.
+"""
 
 from math import log 
 
 def mystere(n):
-    p=log((n),2)
-    T=[]
-    i , aux = 0,n
-    while i<p:
-         T.append(aux%2)
-         aux=aux//2
-         i=i+1
-    return T
+    p=log((n),2)   #O(1)
+    T=[]            #O(1)
+    i , aux = 0,n    #O(1)
+    while i<p:        #O(log(n))
+         T.append(aux%2)   #O(1)
+         aux=aux//2        #O(1)
+         i=i+1            #O(1)
+    return T            #O(1)
+# La complexité 
 
+# La boucle while s'exécute log2(n) fois, et à chaque itération, les opérations effectuées sont de complexité constante O(1).
+# Donc, la complexité totale de la fonction est :
+# O(1) + O(1) + O(1) + O(log(n)) * (O(1) + O(1) + O(1)) + O(1)
+# = O(3) + O(log(n)) * O(3) + O(1)
+# = O(3) + O(3 * log(n)) + O(1)
+# = O(3) + O(log(n)) + O(1)
+# = O(log(n))
+# donc, la complexité  mystere(n) est O(log(n)).
+# Cette complexité est logarithmique car la boucle while s'exécute log2(n) fois,
+# ce qui correspond au nombre de bits nécessaires pour représenter n en binaire.
+
+
+"""
+# La fonction mystere(n) convertit un nombre entier n 
+# en sa représentation binaire inversée sous forme de liste.
+#  Voici une autre solution pour obtenir le même résultat 
+"""
+# solution 1
+def mystere_alternative(n):
+    T = []
+    while n > 0:
+        T.append(n % 2)
+        n = n // 2
+    return T
 
 # autre solution 
-def mystere(n):
-  
-    return T
+def mystere_bin(n):
+        return [int(bit) for bit in bin(n)[2:]] # O(log(n))
+# tout d'abord [ _ for _ in seqs ] est une liste en compréhension 
+
+# et 
+# bin(n)='0b...' retourne une chaîne de caractères représentant 
+# la valeur entière n en binaire.
+# donc bin(n)[2:-1] retourne sans les deux premiers caractères '0b'
+
+    # Q b : La complexité de mystere 
+    # La complexité de la fonction mystere est O(log(n)). 
+    # En effet, la boucle while s'exécute log2(n) fois,
+    #  ce qui correspond au nombre de bits nécessaires 
+    # pour représenter n en binaire.
+# toujeurs sa complexité est O(log(n)) car la represntation binaire
+# d'un nombre entiere est proportionnelle a log(n) de base 2
 
 
-# Q b : La complexité de mystere 
 
+ 
--- pythonFormation101/les fonctions/TDs/TD5_022025/exercice3.py
+++ pythonFormation101/les fonctions/TDs/TD5_022025/exercice3.py
@@ -1,39 +1,55 @@
 
 '''
-fonction fibonacci 
+On rappelle la définition de la suite de Fibonacci :
+   
+     U(n:int) =  match n with 
+           | 0  ->    1
+           | 1   ->    1
+           | n   ->     U(n-1)  + U(n-2) 
 
-U(n) = 0  --> U0=1
-       1      U1 = 1
-       n  Un = Un-1  + Un-2 
-       n = 5 
-       n 5 = f4 + f3 
-             f3 + f2  f1 + f2 
-       f2+f1   f1 + f0 f1 f1 f0
-f0 f1  = f1 , f1+f0
      '''     
-# Q a : 
+# Q a : Évaluer la complexité de la fonction fibo1(n) suivante  
 def fibo(n):
-    u0 , u1 = 1 , 1 
-    for i in range(n):
-        f0 , f1 = f1 , f0 +f1   #~O(1)*n  = O(n)
-    return f0
+    f0 , f1 = 1 , 1  # O(1)
+    for i in range(n): # O(n)
+        f0 , f1 = f1 , f0 +f1  # O(1+1) = O(1)
+    return f0 # O(1)
+# donc la complexité de fibo(n) 
+# = O(1)+O(n)*O(1)+O(1) = O(n)+O(1) = O(n)
+# donc la complexité de fibo(n) est linéaire O(n)
 
 
 
-# Q b : 
+
+# Q b : Écrire une fonction récursive qui retourne la valeur de Fn.
+#  Donner sa complexité. 
 
 """
-     f(n) -> f(n-1 ) + f(n-2)
-                /\         /\
-                n-2 n-3   n-3 n-4 
+La fonction récursive pour calculer la suite de Fibonacci peut être représentée sous forme d'arborescence :
+tou
+cas de base : n= 1 ou 0 retourne 1
+sinon :
+                      f(n) 
+                    /         \
+                f(n-1)        f(n-2)
+                / \             / \
+            f(n-2)  f(n-3)  f(n-3)  f(n-4)
+            ..............................
+            
 
-                n=0   ou n=1 
-                """
+Chaque appel de f(n) génère deux appels récursifs :
+ f(n-1) et f(n-2), 
+ce qui double le nombre d'appels à chaque niveau de l'arborescence.
+La profondeur de l'arborescence est n, donc la complexité est O(2^n).
+"""
 def fibo_recursive(n):
-    if n == 0 or n == 1:  # 
-         return 1
-    return fibo_recursive(n-1) + fibo_recursive(n-2)
+    if n == 0 or n == 1:  # cas de base sert a arreter la recursion
+         return 1          
+    return fibo_recursive(n-1) + fibo_recursive(n-2) # deux appels recursifs
 
-# O(2^N)
+# O(2^N) car chaque appel de f(n) génère deux appels récursifs 
+# complexite exponentielle qui est tres couteuse en terme de temps
+# et d'espace
+
   
  
