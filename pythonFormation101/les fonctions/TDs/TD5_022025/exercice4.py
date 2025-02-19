0233fd49dca51cfb879baeeaba0b296169bf7438
Bekkali Hamza <bakkalihamza2014@gmail.com>
2025-02-19 16:51:17 +0000

    #

--- pythonFormation101/les fonctions/TDs/TD5_022025/exercice4.py
+++ pythonFormation101/les fonctions/TDs/TD5_022025/exercice4.py
@@ -18,13 +18,35 @@ Cet algorithme repose sur les formules suivantes :
 
 def puissanceRec(x, n):
     if n == 0:          # cas de base 
-        return 1
+        return 1      # x^0 = 1 sortir de la récursion dierctement
    
-    if n % 2 == 0:  
+    if n % 2 == 0:  # si n est pair
         a = puissanceRec(x, n//2)
         return a * a
-    else:
+    else: # Si n est impair
         a = puissanceRec(x, (n-1)//2)
         return x * a * a
 # la complexité de cette fonction est O(log(n)) dans le pire des cas
 # car on divise n par 2 à chaque appel récursif
+
+
+
+"""
+Remarque :
+    Pour comprendre comment la pile d'appels récursifs se déploie, 
+    prenons un exemple avec x = 2 et n = 5.
+
+    puissanceRec(2, 5)
+    -> Puisque 5 est impair, on calcule 2 * puissanceRec(2, 2)^2
+        -> puissanceRec(2, 2) -> 2eme appel récursif
+            -> Puisque 2 est pair, on calcule puissanceRec(2, 1)^2
+                -> puissanceRec(2, 1) -> 3eme appel récursif
+                    -> Puisque 1 est impair, on calcule 2 * puissanceRec(2, 0)^2
+                        -> puissanceRec(2, 0) -> 4eme appel récursif
+                            -> Puisque 0 est le cas de base, on retourne 1
+                    -> On retourne 2 * 1^2 = 2   <- 
+            -> On retourne 2^2 = 4
+        -> On retourne 2 * 4^2 = 32
+
+    Ainsi, puissanceRec(2, 5) retourne 32.
+"""
